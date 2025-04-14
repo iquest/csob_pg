c5d189974481ac1c76d5db6fd09742edb008c5a3
Jan Voňavka <jan.vonavka@gmail.com>
2025-04-14 07:57:19 +0200

    message - additional fields

--- test/client_test.rb
+++ test/client_test.rb
@@ -43,16 +43,16 @@ module CsobPaymentGateway
     # gateway tests
     # echo - https://github.com/csob/platebnibrana/wiki/Z%C3%A1kladn%C3%AD-metody#echo-operation
     def test_echo_message_works
-      c = create_client
-      r = c.echo
+      client = create_client
+      response = client.echo
 
-      assert_equal :OK, RESULT_CODES[r.resultCode]
+      assert_equal :OK, RESULT_CODES[response.resultCode]
     end
 
     # payment/process - https://github.com/csob/platebnibrana/wiki/Z%C3%A1kladn%C3%AD-metody#payment-process-operation
     def test_get_url_to_the_gateway
-      c = create_client
-      url = c.process(init_payment(ID_UP_FROM + 1))
+      client = create_client
+      url = client.process(init_payment(ID_UP_FROM + 1))
 
       response = HttpClient.get url
       redirect = response['location']
@@ -62,53 +62,77 @@ module CsobPaymentGateway
       assert_equal(exp, redirect[0...exp.length])
     end
 
+    # payment/init with addition params
+    def test_get_status_of_initialized_payment_with_additional_params
+      client = create_client
+      payment_no = init_payment(ID_UP_FROM + 2,
+                                customer: {
+                                  account: {
+                                    orderHistory: 3,
+                                    suspicious: false
+                                  },
+                                  login: {
+                                    auth: 'guest'
+                                  },
+                                  name: 'John Doe', email: 'john.doe@example.com'
+                                },
+                                order: { billing: { address1: 'aaaa', city: 'bbb', zip: 'aaa', country: 'CZE' },
+                                         type: 'purchase' },
+                                customerId: '1234567890')
+
+      response = client.status(payment_no)
+
+      assert_equal :OK, RESULT_CODES[response.resultCode]
+      assert_equal :payment_initialized, TRANSACTION_LIFECYCLE[response.paymentStatus]
+    end
+
     # payment/status - https://github.com/csob/platebnibrana/wiki/Z%C3%A1kladn%C3%AD-metody#payment-status-operation
     def test_get_status_of_initialized_payment
-      c = create_client
+      client = create_client
       payment_no = init_payment(ID_UP_FROM + 2)
-      r = c.status(payment_no)
+      response = client.status(payment_no)
 
-      assert_equal :OK, RESULT_CODES[r.resultCode]
-      assert_equal :payment_initialized, TRANSACTION_LIFECYCLE[r.paymentStatus]
+      assert_equal :OK, RESULT_CODES[response.resultCode]
+      assert_equal :payment_initialized, TRANSACTION_LIFECYCLE[response.paymentStatus]
     end
 
     # gateway tests - payment actions
     def test_invalid_card_payment
       pay_no = init_payment ID_UP_FROM + 3
-      r = process_payment pay_no, CARD_VISA_AUTH_FAILURE
+      response = process_payment pay_no, CARD_VISA_AUTH_FAILURE
 
-      assert_equal :payment_method_error, RESULT_CODES[r.resultCode]
+      assert_equal :payment_method_error, RESULT_CODES[response.resultCode]
     end
 
     def test_reverse_pending_payment
-      c = create_client
+      client = create_client
       pay_no = init_payment ID_UP_FROM + 4
-      r1 = process_payment pay_no
-      r2 = c.reverse(pay_no)
+      process_response = process_payment pay_no
+      reverse_response = client.reverse(pay_no)
 
-      assert_equal :OK, RESULT_CODES[r1.resultCode], r1.resultMessage
-      assert_equal :OK, RESULT_CODES[r2.resultCode], r2.resultMessage
+      assert_equal :OK, RESULT_CODES[process_response.resultCode], process_response.resultMessage
+      assert_equal :OK, RESULT_CODES[reverse_response.resultCode], reverse_response.resultMessage
     end
 
     def test_close_message_works
-      c = create_client
+      client = create_client
       pay_no = init_payment ID_UP_FROM + 5
-      r = c.close(pay_no)
+      response = client.close(pay_no)
       # unable to induce valid state for closing
-      assert_equal :payment_not_in_valid_state, RESULT_CODES[r.resultCode]
+      assert_equal :payment_not_in_valid_state, RESULT_CODES[response.resultCode]
     end
 
     def test_refund_message_works
-      c = create_client
+      client = create_client
       pay_no = init_payment ID_UP_FROM + 6
-      r = c.refund(pay_no)
+      response = client.refund(pay_no)
       # unable to induce valid state for refund
-      assert_equal :payment_not_in_valid_state, RESULT_CODES[r.resultCode]
+      assert_equal :payment_not_in_valid_state, RESULT_CODES[response.resultCode]
     end
 
     def process_payment(pay_no, cardnumber = CARD_VISA_AUTH_SUCCESS)
-      c = create_client
-      url = c.process(pay_no)
+      client = create_client
+      url = client.process(pay_no)
 
       response = HttpClient.get url
       id = response['location'].split('/').last
@@ -135,34 +159,34 @@ module CsobPaymentGateway
       end
 
       3.times do
-        break if c.status(pay_no).paymentStatus == 7
+        break if client.status(pay_no).paymentStatus == 7
 
         sleep(0.25)
       end
 
-      payment = c.status(pay_no)
+      payment = client.status(pay_no)
       Message::GeneralResponse.new({
-        payId: payment.payId,
-        dttm: payment.dttm,
-        resultCode: payment.resultCode,
-        resultMessage: payment.resultMessage,
-        paymentStatus: payment.paymentStatus,
-        paymentStatusMessage: payment.paymentStatusMessage,
-        authCode: payment.authCode,
-        customerCode: payment.customerCode,
-        statusDetail: payment.statusDetail
-      })
+                                     payId: payment.payId,
+                                     dttm: payment.dttm,
+                                     resultCode: payment.resultCode,
+                                     resultMessage: payment.resultMessage,
+                                     paymentStatus: payment.paymentStatus,
+                                     paymentStatusMessage: payment.paymentStatusMessage,
+                                     authCode: payment.authCode,
+                                     customerCode: payment.customerCode,
+                                     statusDetail: payment.statusDetail
+                                   })
     end
 
-    def init_payment(order_no)
-      c = create_client
+    def init_payment(order_no, **options)
+      client = create_client
       item = Message::Item.new name: 'RailsConf ', quantity: 1, amount: 2000, description: ' RailsConf'
-      r = c.init(order_no: order_no.to_s, total_amount: 2000, items: [item])
+      response = client.init(order_no: order_no.to_s, total_amount: 2000, items: [item], **options)
 
-      assert_equal :OK, RESULT_CODES[r.resultCode], r.resultMessage
-      assert_predicate r, :ok?
-      assert_equal :payment_initialized, TRANSACTION_LIFECYCLE[r.paymentStatus]
-      r.payId
+      assert_equal :OK, RESULT_CODES[response.resultCode], response.resultMessage
+      assert_predicate response, :ok?
+      assert_equal :payment_initialized, TRANSACTION_LIFECYCLE[response.paymentStatus]
+      response.payId
     end
 
     def get_location(uri_as_string)
